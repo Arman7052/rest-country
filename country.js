f872947e7c4f83a1047f5530d6ab9db10a4258c5
Arman7052 <99arman7052@gmail.com>
2023-03-21 22:59:57 +0600

    Display data

--- country.js
+++ country.js
@@ -7,4 +7,25 @@ const loadCountries = () => {
         console.log(data);
         displayCountries(data)})
 };
+
+// Display data //
+const displayCountries = countries => {
+    
+    allCountriesHTML = countries.map(country => getCountryHTML(country));
+    const container = document.getElementById('countries');
+    container.innerHTML = allCountriesHTML.join(' ');
+}
+
+ const getCountryHTML = ({name, flags, region, capital }) =>{
+    return `
+        <div class="country">
+            <img src="${flags.png}">
+            <h2>${name.common}</h2>
+            <h3>${capital}</h3>
+            
+            <p>Continent: ${region}</p>
+            
+        </div>
+    `
+}
 loadCountries();
