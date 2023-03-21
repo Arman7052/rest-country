e70abeaeeac863ca4e8092e2825f2db89a7e7147
Arman7052 <99arman7052@gmail.com>
2023-03-21 22:53:24 +0600

    Fetching data

---
+++ country.js
@@ -0,0 +1,10 @@
+// Fetching data //  
+
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        displayCountries(data)})
+};
+loadCountries();
